8bed1782222532c4e5e4f8bb09e45af59f3941cd
n-path <n.ymmt15@gmail.com>
2020-07-23 08:52:52 +0900

    Eclipseの使い方

---
+++ JavaStudy/chapter1/src/chapter1/Ex1.java
@@ -0,0 +1,16 @@
+package chapter1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		List<String> list = new ArrayList<String>();{
+			list.add("Hello java");
+			System.out.println(list.get(0));
+		}
+
+	}
+
+}
